56f35d22a1162745c79653f99c29e9c1b2080ed8
armcortex <armcortexfpga@gmail.com>
2023-08-14 21:44:17 +0800

    update download_youtube_list

--- download_youtube_list/download_youtube_list.sh
+++ download_youtube_list/download_youtube_list.sh
@@ -1,3 +1,15 @@
-# -N for thread
-threads=`nproc --all`
-yt-dlp -N $threads -o '%(playlist_index)d_%(n_entries)d - %(title)s.%(ext)s' $1
+os=$(uname)
+
+if [[ "$os" == "Linux" ]]; then
+    threads=`nproc --all`
+elif [[ "$os" == "Darwin" ]]; then
+    threads=`sysctl -n hw.ncpu`
+else
+    echo "Unknown OS"
+fi
+
+yt-dlp --concurrent-fragments $threads \
+    --output '%(playlist_index)d_%(n_entries)d - %(title)s.%(ext)s' \
+    --cache-dir ./tmp \
+    --paths ./tmp \
+    $1
